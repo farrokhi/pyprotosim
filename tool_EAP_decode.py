d6756de6ccad20e294188e1db349e942d0b65d1f
Babak Farrokhi <babak@farrokhi.net>
2014-03-01 09:21:29 +0100

    cleanup

--- tool_EAP_decode.py
+++ tool_EAP_decode.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 ##################################################################
 # Copyright (c) 2012, Sergej Srepfler <sergej.srepfler@gmail.com>
-# February 2012 - March 2012
-# Version 0.3, Last change on Oct 24, 2012
+# February 2012 - 
+# Version 0.3.2, Last change on Mar 01, 2014
 # This software is distributed under the terms of BSD license.    
 ##################################################################
 
 # decoding EAP-Payload value into AVPs
 
-from libDiameter import *
 import eap
 import sys
+import logging
 
 if __name__ == "__main__":
     # level for decoding are: DEBUG, INFO, WARNING, ERROR, CRITICAL
     logging.basicConfig(level=logging.DEBUG)
-    LoadDictionary("dictDiameter.xml")
     eap.LoadEAPDictionary("dictEAP.xml")
     msg=sys.argv[1]
     #msg="01020014120A00000F020002000100000D010000"
